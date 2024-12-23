de1c0c5be6521734ffd45e243d2bf6213a55a4fd
chrscalco <xrscalco@gmail.com>
2024-12-23 12:00:32 +0200

    Created the files and added the operator function.

---
+++ script.js
@@ -0,0 +1,28 @@
+const add = function(a,b) {
+    return a + b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const multiply = function(a,b) {
+    return a * b;
+};
+
+const divide = function(a,b) {
+    return a/b;
+};
+
+function operate(a,operator,b) {
+    if (operator === "+") {
+        return add(a,b);
+    } else if (operator === "-") {
+        return subtract(a,b);
+    } else if (operator === "*") {
+        return multiply(a,b);
+    } else {
+        return divide(a,b);
+    }
+};
+
