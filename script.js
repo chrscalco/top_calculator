5baa65e7035e24c10f21ea73ee456fc52a35c3b3
chrscalco <xrscalco@gmail.com>
2024-12-24 19:35:03 +0200

    Created a basic gui.

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-const add = function(a,b) {
-    return a + b;
-};
+//Arrays for calculator
+calcButtons = ["7", "8", "9", "÷", "4", "5", "6", "x", "1", "2", "3", "-", "0", ".", "=", "+"];
 
-const subtract = function(a,b) {
-	return a - b;
-};
 
-const multiply = function(a,b) {
-    return a * b;
-};
-
-const divide = function(a,b) {
-    return a/b;
-};
+//Functions to operate calculator
 
 function operate(a,operator,b) {
     if (operator === "+") {
-        return add(a,b);
+        return a + b;
     } else if (operator === "-") {
-        return subtract(a,b);
+        return a - b;
     } else if (operator === "*") {
-        return multiply(a,b);
+        return a * b;
     } else {
-        return divide(a,b);
+        return a/b;
     }
 };
 
+
+//Calculator dom manipulation and creation
+
+const calculator = document.querySelector("#calculator");
+const screen = document.createElement("div");
+const buttons = document.createElement("div");
+const numberContainer = document.createElement("div");
+const operatorContainer = document.createElement("div");
+const tempText = document.createElement("h1");
+
+tempText.textContent = "7 * 8";
+screen.classList.add("screen");
+buttons.classList.add("buttonContainer");
+
+numberContainer.classList.add("numberContainer");
+operatorContainer.classList.add("operatorContainer");
+
+screen.appendChild(tempText);
+calculator.appendChild(screen);
+buttons.appendChild(numberContainer);
+buttons.appendChild(operatorContainer);
+calculator.appendChild(buttons);
+
+function createCalc(arr) {
+    for (i=0; i<arr.length; i++) {
+        const buttonNum = document.createElement("button");
+        buttonNum.textContent = arr[i];
+        buttonNum.classList.add("calcButton");
+        numberContainer.appendChild(buttonNum);
+    }
+}
+
+createCalc(calcButtons);
