ce3556570b533f389dd10240609c3dc059ed8543
chrscalco <xrscalco@gmail.com>
2024-12-25 08:20:37 +0200

    Calculator mostly in working condition, need to make float numbers.

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 //Arrays and variables for calculator
 const calcButtons = ["7", "8", "9", "÷", "4", "5", "6", "X", "1", "2", "3", "-", "0", ".", "=", "+"];
-let firstOperand;
-let operator;
-let secondOperand;
-let lastOperation;
+let firstOperand = null;
+let operator = null;
+let nextOperato = null;
+let secondOperand = null;
+let lastOperation = null;
+let currentOperation = null;
 
 //Functions to operate and create calculator 
 
 function operate(a,operator,b) {
+    a = parseInt(a);
+    b = parseInt(b);
+
     if (operator === "+") {
         return a + b;
     } else if (operator === "-") {
         return a - b;
-    } else if (operator === "*") {
+    } else if (operator === "X") {
         return a * b;
     } else {
         return a/b;
     }
 };
 
-
+function checkOperator(a, operator , b) {
+    if((a !== null) && (operator !== null) && (b !== null)) {
+        a = operate(a, operator, b);
+        operator = null;
+        b = null;
+    }
+};
 // On press of number adds it to the screen along with last inputs
 
 function updateBotScreen(input, text) {
@@ -32,9 +43,16 @@ function updateBotScreen(input, text) {
     }
 }
 
-function updateTopScreen() {
-    
+function updateTopScreen(input, text) {
+
+    if(text.length >= 9) {
+        return text;
+    } else {
+        text = text + input;
+        return text;
+    }
 }
+
 
 function createCalc(arr) {
     for (i=0; i<arr.length; i++) {
@@ -49,6 +67,12 @@ function createCalc(arr) {
             })
             button.addEventListener("mouseup", () => {
                 button.setAttribute("style", "background-color: #ED6E3E")
+                lastOperation = firstOperand + operator + secondOperand;
+                firstOperand = operate(firstOperand, operator, secondOperand);
+                textBot.textContent = firstOperand;
+                textTop.textContent = lastOperation;
+                operator = null;
+                secondOperand = null;
             })
         } else if ((arr[i] === ".")) { //Float number creation
             button.setAttribute("style", "background-color: #7895A8");
@@ -66,6 +90,9 @@ function createCalc(arr) {
             })
             button.addEventListener("mouseup", () => {
                 button.setAttribute("style", "background-color: #F8C743");
+                checkOperator(firstOperand, operator, secondOperand);
+                operator = button.innerHTML;
+                textBot.textContent = null;
             })
         } else { //Numbers creation
             button.setAttribute("style", "background-color: #7895A8");
@@ -74,8 +101,14 @@ function createCalc(arr) {
             })
             button.addEventListener("mouseup", () => {
                 button.setAttribute("style", "background-color: #7895A8");
-                firstOperand = updateBotScreen(button.innerHTML, textBot.textContent);
-                textBot.textContent = firstOperand;
+                if (operator !== null) {
+                    secondOperand = updateBotScreen(button.innerHTML, textBot.textContent);
+                    textBot.textContent = secondOperand;
+                } else {
+                    firstOperand = updateBotScreen(button.innerHTML, textBot.textContent);
+                    textBot.textContent = firstOperand;
+                }
+
             })
         }
         buttonContainer.appendChild(button);
@@ -93,9 +126,6 @@ const buttonContainer = document.createElement("div");
 const textTop = document.createElement("h1");
 const textBot = document.createElement("h1");
 
-
-textTop.textContent = "";
-textBot.textContent = "";
 screen.classList.add("screen");
 topScreen.classList.add("topScreen");
 botScreen.classList.add("botScreen");
